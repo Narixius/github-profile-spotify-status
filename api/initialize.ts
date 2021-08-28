1e0aad5098c4ec11d5d3d95de9194b1353accc79
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2021-08-28 16:22:26 +0430

    feat: call back url and documentation implementation

--- api/initialize.ts
+++ api/initialize.ts
@@ -1,5 +1,8 @@
 import request from 'request';
 
+let baseURL = `${process.env.VERCEL_ENV == 'development' ? 'http://' : 'https://'}${process.env.VERCEL_URL}`;
+let redirectURL = `${baseURL}/init`;
+
 export default (req, res) => {
   if (req.query.code)
     return request(
@@ -11,19 +14,42 @@ export default (req, res) => {
           client_secret: process.env.SPOTIFY_CLIENT_SECRET,
           grant_type: 'authorization_code',
           code: req.query.code,
-          redirect_uri: process.env.VERCEL_URL,
+          redirect_uri: redirectURL,
         },
       },
       (err, _, body) => {
-        if (err || !body || !JSON.parse(body) || !JSON.parse(body).SPOTIFY_REFRESH_TOKEN)
-          return res.send(`unable to generate refresh token. <a href="${process.env.VERCEL_URL}">Try Again</a>`);
-        return res.send(`Your refresh token is: <pre>${JSON.parse(body).SPOTIFY_REFRESH_TOKEN}</pre>`);
+        if (err || !body || !JSON.parse(body) || !JSON.parse(body).refresh_token)
+          return res.send(
+            `<p>Unable to generate refresh token.</p><pre>${body}</pre> <br/> <a href="${redirectURL}">Try Again</a>`
+          );
+        return res.send(`
+		<p style="color: green;"><b>Refesh token generated successfully</b></p>
+		<p>Your refresh token is:</p>
+		<pre>${JSON.parse(body).refresh_token}</pre>
+		<p>Now you have to add this refresh token to your vercel project environment variables</p>
+		<ol>
+			<li>Open <a href="https://vercel.com/dashboard">Vercel Dashboard</a></li>
+			<li>Select your project</li>
+			<li>From the top navigation, go to settings</li>
+			<li>On the side abr select <b>Environment Variables</b></li>
+			<li>Scroll down and find the <b>SPOTIFY_REFRESH_TOKEN</b> variables, by clicking on the right dots, edit this variable and change it to the code above</li>
+		</ol>
+		<p>Then open <a href="${baseURL}">Homepage</a> and see your Spotify Status </p>
+		`);
       }
     );
   else {
     return res.send(
-      `<p>Your redirect url is: <pre>${process.env.VERCEL_URL}</pre></p>
-	  <a href="https://accounts.spotify.com/en/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=user-read-currently-playing,user-read-recently-played,user-top-read&redirect_uri=${process.env.VERCEL_URL}">Get Refresh Token</a>`
+      `<p>Your redirect url is: <pre>${redirectURL}</pre></p>
+      <p>Please add the redirect url above to your spotify app settings on <b>Redirect URIs</b> sections.</p>
+	  <ol>
+	  	<li>Open <a href="https://developer.spotify.com/dashboard">Spotify Developer Dashboard</a></li>
+	  	<li>Select your app</li>
+		<li>Click on Edit Settings button</li>
+		<li>Add the redirect URL above to the <b>Redirect URIs</b> section</li>
+	  </ol>
+	  <p>Then try to generate refresh token using the link bellow</p>
+	  <a href="https://accounts.spotify.com/en/authorize?response_type=code&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=user-read-currently-playing,user-read-recently-played,user-top-read&redirect_uri=${redirectURL}">Get Refresh Token</a>`
     );
   }
 };
